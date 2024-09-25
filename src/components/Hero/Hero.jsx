d083403fb4116f82ebaffd89ea9173ce33b96e88
Snail Mail <kapilkapse007@gmail.com>
2024-09-26 03:12:41 +0530

    test 1.2

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import styles from "./Hero.module.css"
-import HeroImg from "../../assets/HeroImg.png"
+import HeroImg from "../../assets/HeroImg.svg"
 
 const Hero = () => {
   return (
     <div className={styles.hero}>
         <div >
-            <h1>100 Thousonad Songs, ad-free</h1>
+            <h1>100 Thousand Songs, ad-free</h1>
             <h1>Over thousands podcast episodes</h1>
         </div>
             <img width={212}  src={HeroImg} alt="Headphone" />
